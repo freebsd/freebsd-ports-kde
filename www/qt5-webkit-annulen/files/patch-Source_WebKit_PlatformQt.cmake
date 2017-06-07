bd1f434287fe12342ca5db489fed4154f5e99722
Tobias C. Berner <tcberner@FreeBSD.org>
2017-06-07 21:37:17 +0200

    Update www/qt5-webkit-annulen to the newest release snapshot.
    * modify ECMGeneratePkgConfigFile to also insert "Description" into the generated pkg-config file, as pgkconf was rejecting it otherwise.

--- www/qt5-webkit-annulen/files/patch-Source_WebKit_PlatformQt.cmake
+++ www/qt5-webkit-annulen/files/patch-Source_WebKit_PlatformQt.cmake
@@ -1,11 +1,30 @@
---- Source/WebKit/PlatformQt.cmake.orig	2017-05-02 08:48:51 UTC
+--- Source/WebKit/PlatformQt.cmake.orig	2017-06-02 18:33:03 UTC
 +++ Source/WebKit/PlatformQt.cmake
-@@ -480,7 +480,7 @@ if (NOT MACOS_BUILD_FRAMEWORKS)
+@@ -481,8 +481,9 @@ if (NOT MACOS_BUILD_FRAMEWORKS)
+         BASE_NAME Qt5WebKit
          DEPS "${WEBKIT_PKGCONGIG_DEPS}"
          FILENAME_VAR WebKit_PKGCONFIG_FILENAME
++        DESCRIPTION "Qt WebKit module"
      )
 -    set(ECM_PKGCONFIG_INSTALL_DIR "${LIB_INSTALL_DIR}/pkgconfig" CACHE PATH "The directory where pkgconfig will be installed to.")
 +    set(ECM_PKGCONFIG_INSTALL_DIR "${CMAKE_INSTALL_PREFIX}/libdata/pkgconfig" CACHE PATH "The directory where pkgconfig will be installed to.")
      install(FILES ${WebKit_PKGCONFIG_FILENAME} DESTINATION ${ECM_PKGCONFIG_INSTALL_DIR} COMPONENT Data)
  endif ()
  
+@@ -577,7 +578,7 @@ set(WebKitWidgets_SOURCES
+ set(WebKitWidgets_SYSTEM_INCLUDE_DIRECTORIES
+     ${Qt5Gui_INCLUDE_DIRS}
+     ${Qt5Network_INCLUDE_DIRS}
+-    ${Qt5Widgets_INCLUDE_DIRS}
++    {Qt5Widgets_INCLUDE_DIRS}
+ )
+ 
+ set(WebKitWidgets_LIBRARIES
+@@ -671,6 +672,7 @@ if (NOT MACOS_BUILD_FRAMEWORKS)
+         BASE_NAME Qt5WebKitWidgets
+         DEPS "${WEBKITWIDGETS_PKGCONFIG_DEPS}"
+         FILENAME_VAR WebKitWidgets_PKGCONFIG_FILENAME
++        DESCRIPTION "Qt WebKitWidgets module"
+     )
+     install(FILES ${WebKitWidgets_PKGCONFIG_FILENAME} DESTINATION ${ECM_PKGCONFIG_INSTALL_DIR} COMPONENT Data)
+ endif ()
