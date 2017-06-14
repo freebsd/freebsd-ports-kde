268357d4cd9f319f9ca51ade4d7793fd52443b5c
Adriaan de Groot <groot@kde.org>
2017-06-14 22:52:17 +0200

    CMake: use specialized <platform>-CXX.cmake file

---
+++ devel/cmake/files/patch-Modules_Platform_FreeBSD-CXX.cmake
@@ -0,0 +1,6 @@
+--- /dev/null	2017-06-14 21:11:00.000000000 +0200
++++ Modules/Platform/FreeBSD-CXX.cmake	2017-06-14 20:59:51.847698783 +0200
+@@ -0,0 +1,3 @@
++if(NOT CMAKE_CXX_COMPILER_NAMES)
++  set(CMAKE_CXX_COMPILER_NAMES c++)
++endif()
