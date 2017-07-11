77f1ab519c455ea743123711867d8c83b6dfb635
Tobias C. Berner <tcberner@FreeBSD.org>
2017-07-11 21:38:58 +0200

    Update KDE Applications to 17.04.3

--- multimedia/kdenlive/files/patch-src_lib_external_media__ctrl_mediactrl.c
+++ multimedia/kdenlive/files/patch-src_lib_external_media__ctrl_mediactrl.c
@@ -1,10 +1,12 @@
---- src/lib/external/media_ctrl/mediactrl.c.orig	2017-04-09 08:27:56 UTC
+--- src/lib/external/media_ctrl/mediactrl.c.orig	2017-07-11 19:20:19 UTC
 +++ src/lib/external/media_ctrl/mediactrl.c
-@@ -24,7 +24,6 @@
+@@ -24,7 +24,9 @@
  #include <sys/types.h>
  #include <sys/stat.h>
  #include <sys/time.h>
--#include <asm/types.h>
++#if defined(Q_OS_LINUX)
+ #include <asm/types.h>
++#endif
  #include <fcntl.h>
  #include <unistd.h>
  #include <stdint.h>
