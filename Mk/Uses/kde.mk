27213cce4ef936d35885ebe2e9dfd953a0ce27ca
Tobias C. Berner <tcberner@FreeBSD.org>
2017-07-22 00:15:15 +0200

    Set proper branch

--- Mk/Uses/kde.mk
+++ Mk/Uses/kde.mk
@@ -79,7 +79,7 @@ KDE_FRAMEWORKS_VERSION?=	5.36.0
 KDE_FRAMEWORKS_BRANCH?= 	stable
 
 KDE_APPLICATIONS_VERSION?=	17.07.80
-KDE_APPLICATIONS_BRANCH?=	stable
+KDE_APPLICATIONS_BRANCH?=	unstable
 # Upstream moves old software to Attic/. Specify the newest applications release there.
 # Only the major version is used for the comparison.
 _KDE_APPLICATIONS_ATTIC_VERSION=	15.12.3
