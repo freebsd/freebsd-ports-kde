10a442a79a1a25888abacf24eefec2f7be0adf94
Tobias C. Berner <tcberner@FreeBSD.org>
2017-10-29 10:07:34 +0100

    Add poppler-qt5 fix from branches/plasma5

---
+++ graphics/poppler/files/patch-qt5_src_poppler-form.h
@@ -0,0 +1,10 @@
+--- qt5/src/poppler-form.h.orig	2017-08-25 08:10:02 UTC
++++ qt5/src/poppler-form.h
+@@ -22,6 +22,7 @@
+ #ifndef _POPPLER_QT5_FORM_H_
+ #define _POPPLER_QT5_FORM_H_
+ 
++#include <time.h>
+ #include <QtCore/QRectF>
+ #include <QtCore/QStringList>
+ #include <QtCore/QSharedPointer>
