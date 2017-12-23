freebsd/freebsd-ports-kde

commit 3e8f56d5c8826f00df9601f32c584bf066635137

Update devel/qt5-qmake

Mk/bsd.qt.mk
@@ -176,6 +176,7 @@ CONFIGURE_ARGS+=-verbose
 .  if ${_QT_VERSION:M4*}
 _EXTRA_PATCHES_QT4=	${.CURDIR:H:H}/devel/${_QT_RELNAME}/files/extrapatch-src-corelib-global-qglobal.h \
 					${.CURDIR:H:H}/devel/${_QT_RELNAME}/files/extrapatch-libtool
+		${.CURDIR:H:H}/devel/${_QT_RELNAME}/files/extrapatch-config.tests-unix-compile.test \
 # Patch in proper name for armv6 architecture: https://gcc.gnu.org/ml/gcc-patches/2015-06/msg01679.html
 _EXTRA_PATCHES_QT4+=	${.CURDIR:H:H}/devel/${_QT_RELNAME}/files/extrapatch-armv6
 .  else
@@ -183,7 +184,6 @@ _EXTRA_PATCHES_QT5=	${.CURDIR:H:H}/devel/${_QT_RELNAME}/files/extrapatch-mkspecs
 					${.CURDIR:H:H}/devel/${_QT_RELNAME}/files/extrapatch-mkspecs_features_qt__module.prf
 .  endif
 EXTRA_PATCHES?=	${.CURDIR:H:H}/devel/${_QT_RELNAME}/files/extrapatch-configure \
-		${.CURDIR:H:H}/devel/${_QT_RELNAME}/files/extrapatch-config.tests-unix-compile.test \
 		${_EXTRA_PATCHES_QT4} ${_EXTRA_PATCHES_QT5}
 . endif
 

devel/qt5-qmake/Makefile
@@ -2,7 +2,6 @@
 
 PORTNAME=	qmake
 DISTVERSION=	${QT5_VERSION}
-PORTREVISION=	3
 CATEGORIES=	devel
 PKGNAMEPREFIX=	qt5-
 
@@ -15,22 +14,23 @@ SHEBANG_FILES=	util/harfbuzz/update-harfbuzz \
 		util/unicode/x11/makeencodings \
 		src/3rdparty/freetype/src/tools/afblue.pl \
 		mkspecs/features/data/mac/objc_namespace.sh \
-		mkspecs/macx-ios-clang/ios_destinations.sh \
-		mkspecs/macx-ios-clang/ios_devices.pl
+		mkspecs/features/uikit/devices.pl \
+		mkspecs/features/uikit/device_destinations.sh
+
 QT_DIST=	base
 REINPLACE_ARGS=	-i ""
 HAS_CONFIGURE=	yes
 # Disable everything to install minimal qconfig.pri.
 CONFIGURE_ARGS=	-no-accessibility -no-openssl -no-gui -no-cups \
 		-no-iconv -no-icu -no-dbus -no-xcb -no-opengl \
-		-no-glib -no-audio-backend -no-fontconfig \
-		-no-gtk -no-xinput2 -no-xrender \
-		-no-evdev -no-xkbcommon -no-alsa \
+		-no-glib -no-fontconfig \
+		-no-gtk -no-xinput2 \
+		-no-evdev -no-xkbcommon \
 		-no-freetype -no-gif -no-harfbuzz -no-libjpeg \
-		-no-libpng -no-pulseaudio -no-widgets
+		-no-libpng -no-widgets
 # Features yet to be removed from qconfig.pri.
 TBR_CONFIG=	concurrent|inotify|xlib
-QMAKESPEC=	${WRKSRC}/mkspecs/freebsd-${QMAKE_COMPILER}
+QMAKESPEC=	freebsd-${QMAKE_COMPILER}
 INSTALL_TARGET=	sub-qmake-qmake-aux-pro-install_subtargets install_mkspecs
 
 BUILD_WRKSRC=	${WRKSRC}/${PORTNAME}

devel/qt5-qmake/files/patch-configure
@@ -1,25 +1,0 @@
---- configure.orig	2016-02-24 19:25:11 UTC
-+++ configure
-@@ -4108,11 +4108,11 @@ if [ '!' -e "$outpath/bin/qmake" ]; then
-         echo "QT_MAJOR_VERSION = $QT_MAJOR_VERSION" >> "$mkfile"
-         echo "QT_MINOR_VERSION = $QT_MINOR_VERSION" >> "$mkfile"
-         echo "QT_PATCH_VERSION = $QT_PATCH_VERSION" >> "$mkfile"
--        echo "EXTRA_CFLAGS = $EXTRA_CFLAGS" >> "$mkfile"
--        echo "EXTRA_CXXFLAGS = $EXTRA_CXXFLAGS" >> "$mkfile"
-+        echo "EXTRA_CFLAGS = $EXTRA_CFLAGS $CFLAGS" >> "$mkfile"
-+        echo "EXTRA_CXXFLAGS = $EXTRA_CXXFLAGS $CXXFLAGS" >> "$mkfile"
-         echo "QTOBJS =" $EXTRA_OBJS >> "$mkfile"
-         echo "QTSRCS =" $EXTRA_SRCS >> "$mkfile"
--        echo "LFLAGS = $EXTRA_LFLAGS" >> "$mkfile"
-+        echo "LFLAGS = $EXTRA_LFLAGS $LDFLAGS" >> "$mkfile"
-         echo "EXEEXT = $EXEEXT" >> "$mkfile"
-         echo "RM_F = rm -f" >> "$mkfile"
-         echo "RM_RF = rm -rf" >> "$mkfile"
-@@ -4131,6 +4131,7 @@ if [ '!' -e "$outpath/bin/qmake" ]; then
-             fi
-         fi
-     done
-+    exit 0
- 
-     if [ "$OPT_VERBOSE" = yes ]; then
-         # Show the output of make

devel/qt5/files/extrapatch-config.tests-unix-compile.test
@@ -1,11 +1,0 @@
---- config.tests/unix/compile.test.orig	2016-05-12 13:16:04.782542000 +0200
-+++ config.tests/unix/compile.test	2016-05-12 13:16:17.510837000 +0200
-@@ -14,7 +14,7 @@
- LFLAGS="$SYSROOT_FLAG"
- INCLUDEPATH=""
- CFLAGS="$SYSROOT_FLAG"
--CXXFLAGS="$SYSROOT_FLAG"
-+CXXFLAGS="$CXXFLAGS $SYSROOT_FLAG"
- MAC_ARCH_CXXFLAGS=""
- MAC_ARCH_LFLAGS=""
- while [ "$#" -gt 0 ]; do

devel/qt5/files/extrapatch-configure
@@ -1,177 +1,18 @@
-* The "build qmake" chunk is required for qtbase ports other than devel/qmake5
-  to use the already built qmake instead of always building their own copy.
-
-* All the other chunks are part of the changes required to avoid ports/194088.
-
-  We want to avoid passing -I${LOCALBASE}/include and -L${LOCALBASE}/lib to the
-  compiler since in Qt5 at least for the Qt ports themselves those parameters
-  will be passed before the build directories, and consequently an older
-  installed version will be used when building a new Qt port and cause
-  problems. See QTBUG-40825 for a discussion we have started upstream.
-
-  This part of the solution comprises both filtering the output of the
-  pkg-config calls so it does not contain those parameters as well as
-  changing the default value of DEFAULT_LIBDIRS in bsd.qt.mk (in the
-  qtbase-post-patch target). It is used in the pkg-config filtering as well
-  as the value of QMAKE_DEFAULT_LIBDIRS in mkspecs/qconfig.pri (which qmake
-  uses to filter out those same paths when processing .pro files).
-
---- configure.orig      2015-03-16 17:16:10.000000000 +0100
-+++ configure           2016-04-18 12:35:38.595937000 +0200
-@@ -3878,7 +3878,7 @@
+--- configure.orig	2017-07-14 14:53:54.062844000 +0200
++++ configure	2017-07-14 14:55:47.269845000 +0200
+@@ -713,6 +713,7 @@
  }
-
+ 
  # build qmake
--if true; then ###[ '!' -f "$outpath/bin/qmake" ];
-+if [ '!' -e "$outpath/bin/qmake" ]; then
++if [ '!' -e "$outpath/qmake/qmake" ] ; then 
      echo "Creating qmake..."
-
      mkdir -p "$outpath/qmake" || exit
-@@ -5045,8 +5045,8 @@
-                 SQLITE_AUTODETECT_FAILED="no"
-                 if [ "$CFG_SQLITE" = "system" ]; then
-                     if [ -n "$PKG_CONFIG" ] && $PKG_CONFIG --exists sqlite3 2>/dev/null; then
--                        QT_CFLAGS_SQLITE=`$PKG_CONFIG --cflags sqlite3 2>/dev/null`
--                        QT_LFLAGS_SQLITE=`$PKG_CONFIG --libs sqlite3 2>/dev/null`
-+                        QT_CFLAGS_SQLITE=`$PKG_CONFIG --cflags sqlite3 2>/dev/null | filterIncludeOptions`
-+                        QT_LFLAGS_SQLITE=`$PKG_CONFIG --libs sqlite3 2>/dev/null | filterLibraryOptions`
-                     else
-                         QT_CFLAGS_SQLITE=
-                         QT_LFLAGS_SQLITE="-lsqlite3 -lz"
-@@ -5149,8 +5149,8 @@
- # runtime: no detection (cannot fail), load libdbus-1 at runtime
- if [ "$CFG_DBUS" = "auto" ] || [ "$CFG_DBUS" = "linked" ]; then
-     if [ -n "$PKG_CONFIG" ] && $PKG_CONFIG --atleast-version="$MIN_DBUS_1_VERSION" dbus-1 2>/dev/null; then
--        QT_CFLAGS_DBUS=`$PKG_CONFIG --cflags dbus-1 2>/dev/null`
--        QT_LIBS_DBUS=`$PKG_CONFIG --libs dbus-1 2>/dev/null`
-+        QT_CFLAGS_DBUS=`$PKG_CONFIG --cflags dbus-1 2>/dev/null | filterIncludeOptions`
-+        QT_LIBS_DBUS=`$PKG_CONFIG --libs dbus-1 2>/dev/null | filterLibraryOptions`
-     else
-         QT_LIBS_DBUS="-ldbus-1"
+ 
+@@ -814,6 +815,7 @@
+         fi
+         echo "Done."
      fi
-@@ -5201,8 +5201,8 @@
- # auto-detect Glib support
- if [ "$CFG_GLIB" != "no" ]; then
-     if [ -n "$PKG_CONFIG" ]; then
--        QT_CFLAGS_GLIB=`$PKG_CONFIG --cflags glib-2.0 gthread-2.0 2>/dev/null`
--        QT_LIBS_GLIB=`$PKG_CONFIG --libs glib-2.0 gthread-2.0 2>/dev/null`
-+        QT_CFLAGS_GLIB=`$PKG_CONFIG --cflags glib-2.0 gthread-2.0 2>/dev/null | filterIncludeOptions`
-+        QT_LIBS_GLIB=`$PKG_CONFIG --libs glib-2.0 gthread-2.0 2>/dev/null | filterLibraryOptions`
-     fi
-     if compileTest unix/glib "Glib" $QT_CFLAGS_GLIB $QT_LIBS_GLIB; then
-         CFG_GLIB=yes
-@@ -5225,9 +5225,9 @@
- # auto-detect GTK style support
- if [ "$CFG_GLIB" = "yes" -a "$CFG_GTK" != "no" ]; then
-     if [ -n "$PKG_CONFIG" ]; then
--        QT_CFLAGS_QGTK3=`$PKG_CONFIG --cflags gtk+-3.0 2>/dev/null`
--        QT_LIBS_QGTK3=`$PKG_CONFIG --libs gtk+-3.0 2>/dev/null`
--        QT_LIBS_QGOBJECT=`$PKG_CONFIG --libs gobject-2.0 2>/dev/null`
-+        QT_CFLAGS_QGTK3=`$PKG_CONFIG --cflags gtk+-3.0 2>/dev/null | filterIncludeOptions`
-+        QT_LIBS_QGTK3=`$PKG_CONFIG --libs gtk+-3.0 2>/dev/null | filterLibraryOptions`
-+        QT_LIBS_QGOBJECT=`$PKG_CONFIG --libs gobject-2.0 2>/dev/null | filterLibraryOptions`
-     fi
-     if [ -n "$QT_CFLAGS_QGTK3" ] ; then
-         CFG_GTK=yes
-@@ -5272,8 +5272,8 @@
- # Auto-detect PulseAudio support
- if [ "$CFG_PULSEAUDIO" != "no" ]; then
-     if [ -n "$PKG_CONFIG" ]; then
--        QT_CFLAGS_PULSEAUDIO=`$PKG_CONFIG --cflags libpulse '>=' 0.9.10 libpulse-mainloop-glib 2>/dev/null`
--        QT_LIBS_PULSEAUDIO=`$PKG_CONFIG --libs libpulse '>=' 0.9.10 libpulse-mainloop-glib 2>/dev/null`
-+        QT_CFLAGS_PULSEAUDIO=`$PKG_CONFIG --cflags libpulse '>=' 0.9.10 libpulse-mainloop-glib 2>/dev/null | filterIncludeOptions`
-+        QT_LIBS_PULSEAUDIO=`$PKG_CONFIG --libs libpulse '>=' 0.9.10 libpulse-mainloop-glib 2>/dev/null | filterLibraryOptions`
-     fi
-     if compileTest unix/pulseaudio "PulseAudio" $QT_CFLAGS_PULSEAUDIO $QT_LIBS_PULSEAUDIO; then
-         CFG_PULSEAUDIO=yes
-@@ -5436,8 +5436,8 @@
- # auto-detect FontConfig support
- if [ "$CFG_FONTCONFIG" != "no" ]; then
-     if [ -n "$PKG_CONFIG" ] && $PKG_CONFIG --exists fontconfig --exists freetype2 2>/dev/null; then
--        QT_CFLAGS_FONTCONFIG=`$PKG_CONFIG --cflags fontconfig --cflags freetype2 2>/dev/null`
--        QT_LIBS_FONTCONFIG=`$PKG_CONFIG --libs fontconfig --libs freetype2 2>/dev/null`
-+        QT_CFLAGS_FONTCONFIG=`$PKG_CONFIG --cflags fontconfig --cflags freetype2 2>/dev/null | filterIncludeOptions`
-+        QT_LIBS_FONTCONFIG=`$PKG_CONFIG --libs fontconfig --libs freetype2 2>/dev/null | filterLibraryOptions`
-     else
-         QT_CFLAGS_FONTCONFIG=
-         QT_LIBS_FONTCONFIG="-lfreetype -lfontconfig"
-@@ -5192,9 +5192,9 @@
-
- if [ "$CFG_LIBUDEV" != "no" ]; then
-     if [ -n "$PKG_CONFIG" ] && $PKG_CONFIG --exists libudev 2>/dev/null; then
--        QMAKE_INCDIR_LIBUDEV=`$PKG_CONFIG --cflags-only-I libudev 2>/dev/null | sed -e 's,^-I,,g' -e 's, -I, ,g'`
--        QMAKE_LIBS_LIBUDEV=`$PKG_CONFIG --libs libudev 2>/dev/null`
--        QMAKE_CFLAGS_LIBUDEV=`$PKG_CONFIG --cflags libudev 2>/dev/null`
-+        QMAKE_INCDIR_LIBUDEV=`$PKG_CONFIG --cflags-only-I libudev 2>/dev/null | filterIncludeOptions | sed -e 's,^-I,,g' -e 's, -I, ,g'`
-+        QMAKE_LIBS_LIBUDEV=`$PKG_CONFIG --libs libudev 2>/dev/null | filterLibraryOptions`
-+        QMAKE_CFLAGS_LIBUDEV=`$PKG_CONFIG --cflags libudev 2>/dev/null | filterIncludeOptions`
-         QMakeVar set QMAKE_INCDIR_LIBUDEV "$QMAKE_INCDIR_LIBUDEV"
-         QMakeVar set QMAKE_LIBS_LIBUDEV "$QMAKE_LIBS_LIBUDEV"
-     fi
-@@ -5333,8 +5333,8 @@
-
- if [ "$CFG_XCB" != "no" ]; then
-     if [ -n "$PKG_CONFIG" ] && $PKG_CONFIG --exists "xcb >= 1.5" 2>/dev/null; then
--        QMAKE_CFLAGS_XCB="`$PKG_CONFIG --cflags xcb 2>/dev/null`"
--        QMAKE_LIBS_XCB="`$PKG_CONFIG --libs xcb 2>/dev/null`"
-+        QMAKE_CFLAGS_XCB="`$PKG_CONFIG --cflags xcb 2>/dev/null | filterIncludeOptions`"
-+        QMAKE_LIBS_XCB="`$PKG_CONFIG --libs xcb 2>/dev/null | filterLibraryOptions`"
-     fi
-     if [ -n "$PKG_CONFIG" ] && $PKG_CONFIG --exists "x11" 2> /dev/null; then
-         QMAKE_X11_PREFIX="`$PKG_CONFIG --variable=prefix x11`"
-@@ -5631,16 +5631,16 @@
-             CFG_XCB="system"
-             if [ -n "$PKG_CONFIG" ] && $PKG_CONFIG --exists "xcb >= 1.5" 2>/dev/null; then
-                 XCB_PACKAGES="xcb xcb-shm xcb-sync xcb-xfixes xcb-randr xcb-image xcb-keysyms xcb-icccm xcb-shape"
--                QMAKE_CFLAGS_XCB="`$PKG_CONFIG --cflags $XCB_PACKAGES 2>/dev/null`"
--                QMAKE_LIBS_XCB="`$PKG_CONFIG --libs $XCB_PACKAGES 2>/dev/null`"
-+                QMAKE_CFLAGS_XCB="`$PKG_CONFIG --cflags $XCB_PACKAGES 2>/dev/null | filterIncludeOptions`"
-+                QMAKE_LIBS_XCB="`$PKG_CONFIG --libs $XCB_PACKAGES 2>/dev/null | filterLibraryOptions`"
-             fi
-
-             # libxcb version 1.10 was the first version that enables xcb-xkb by default,
-             # therefore the minimal xcb-xkb version we support is 1.10
-             CFG_XKB=no
-             if $PKG_CONFIG --exists "xcb-xkb >= 1.10" 2>/dev/null; then
--                QMAKE_CFLAGS_XKB="`$PKG_CONFIG --cflags xcb xcb-xkb 2>/dev/null`"
--                QMAKE_LIBS_XKB="`$PKG_CONFIG --libs xcb xcb-xkb 2>/dev/null`"
-+                QMAKE_CFLAGS_XKB="`$PKG_CONFIG --cflags xcb xcb-xkb 2>/dev/null | filterIncludeOptions`"
-+                QMAKE_LIBS_XKB="`$PKG_CONFIG --libs xcb xcb-xkb 2>/dev/null | filterLibraryOptions`"
-                 if compileTest qpa/xcb-xkb "xcb-xkb" $QMAKE_CFLAGS_XKB $QMAKE_LIBS_XKB; then
-                     CFG_XKB=yes
-                 fi
-@@ -5416,8 +5416,8 @@
-
- if [ "$CFG_DIRECTFB" != "no" ]; then
-     if [ -n "$PKG_CONFIG" ] && $PKG_CONFIG --exists directfb 2>/dev/null; then
--        QMAKE_CFLAGS_DIRECTFB=`$PKG_CONFIG --cflags directfb 2>/dev/null`
--        QMAKE_LIBS_DIRECTFB=`$PKG_CONFIG --libs directfb 2>/dev/null`
-+        QMAKE_CFLAGS_DIRECTFB=`$PKG_CONFIG --cflags directfb 2>/dev/null | filterIncludeOptions`
-+        QMAKE_LIBS_DIRECTFB=`$PKG_CONFIG --libs directfb 2>/dev/null | filterLibraryOptions`
-         if compileTest qpa/directfb "DirectFB" $QMAKE_CFLAGS_DIRECTFB $QMAKE_LIBS_DIRECTFB; then
-             CFG_DIRECTFB=yes
-         elif [ "$CFG_DIRECTFB" = "yes" ] && [ "$CFG_CONFIGURE_EXIT_ON_ERROR" = "yes" ]; then
-@@ -5776,8 +5776,8 @@
-     if [ "$CFG_XKBCOMMON" != "no" ] && [ "$CFG_XKBCOMMON" != "qt" ]; then
-         # Check if there is a suitable system-wide xkbcommon
-         if [ -n "$PKG_CONFIG" ] && $PKG_CONFIG --exists "xkbcommon xkbcommon-x11 >= $MIN_REQ_XKBCOMMON" 2>/dev/null; then
--            QMAKE_CFLAGS_XKBCOMMON="`$PKG_CONFIG --cflags xkbcommon xkbcommon-x11 2>/dev/null`"
--            QMAKE_LIBS_XKBCOMMON="`$PKG_CONFIG --libs xkbcommon xkbcommon-x11 2>/dev/null`"
-+            QMAKE_CFLAGS_XKBCOMMON="`$PKG_CONFIG --cflags xkbcommon xkbcommon-x11 2>/dev/null | filterIncludeOptions`"
-+            QMAKE_LIBS_XKBCOMMON="`$PKG_CONFIG --libs xkbcommon xkbcommon-x11 2>/dev/null | filterLibraryOptions`"
-
-             QMakeVar set QMAKE_CFLAGS_XKBCOMMON "$QMAKE_CFLAGS_XKBCOMMON"
-             QMakeVar set QMAKE_LIBS_XKBCOMMON "$QMAKE_LIBS_XKBCOMMON"
-@@ -5830,9 +5830,9 @@
-     fi
-
-     if [ -n "$PKG_CONFIG" ] && $PKG_CONFIG --exists egl 2>/dev/null; then
--        QMAKE_INCDIR_EGL=`$PKG_CONFIG --cflags-only-I egl 2>/dev/null | sed -e 's,^-I,,g' -e 's, -I, ,g'`
--        QMAKE_LIBS_EGL=`$PKG_CONFIG --libs egl 2>/dev/null`
--        QMAKE_CFLAGS_EGL=`$PKG_CONFIG --cflags egl 2>/dev/null`
-+        QMAKE_INCDIR_EGL=`$PKG_CONFIG --cflags-only-I egl 2>/dev/null | filterIncludeOptions | sed -e 's,^-I,,g' -e 's, -I, ,g'`
-+        QMAKE_LIBS_EGL=`$PKG_CONFIG --libs egl 2>/dev/null | filterLibraryOptions`
-+        QMAKE_CFLAGS_EGL=`$PKG_CONFIG --cflags egl 2>/dev/null | filterIncludeOptions`
-         QMakeVar set QMAKE_INCDIR_EGL "$QMAKE_INCDIR_EGL"
-         QMakeVar set QMAKE_LIBS_EGL "$QMAKE_LIBS_EGL"
-         QMakeVar set QMAKE_CFLAGS_EGL "`echo " $QMAKE_CFLAGS_EGL " | sed -e 's, -I[^ ]* , ,g;s,^ ,,;s, $,,'`"
++fi # Creating qmake
+ 
+ #-------------------------------------------------------------------------------
+ # create a qt.conf for the Qt build tree itself

devel/qt5/files/extrapatch-mkspecs_features_create__cmake.prf
@@ -12,66 +12,57 @@ to fix up the paths so that cmake files still land in the the "correct" location
 in /usr/local/lib/cmake -- as this seemed to be the least intrusive method.
 
 
-
---- mkspecs/features/create_cmake.prf.orig	2016-05-11 09:21:02.000000000 +0200
-+++ mkspecs/features/create_cmake.prf	2016-06-03 18:21:42.389363000 +0200
-@@ -48,7 +52,7 @@
+--- mkspecs/features/create_cmake.prf.orig	2017-06-28 11:54:29.000000000 +0200
++++ mkspecs/features/create_cmake.prf	2017-07-14 15:04:51.254262000 +0200
+@@ -48,7 +48,7 @@
  }
-
+ 
  CMAKE_INCLUDE_DIR = $$cmakeRelativePath($$[QT_INSTALL_HEADERS], $$[QT_INSTALL_PREFIX])
 -contains(CMAKE_INCLUDE_DIR, "^\\.\\./.*") {
 +contains(CMAKE_INCLUDE_DIR, "^\\.\\./.*")|freebsd {
      CMAKE_INCLUDE_DIR = $$[QT_INSTALL_HEADERS]/
      CMAKE_INCLUDE_DIR_IS_ABSOLUTE = True
  }
-@@ -56,7 +60,7 @@
+@@ -56,7 +56,7 @@
  !exists($$first(QT.$${MODULE}_private.includes)): CMAKE_NO_PRIVATE_INCLUDES = true
-
+ 
  CMAKE_LIB_DIR = $$cmakeRelativePath($$[QT_INSTALL_LIBS], $$[QT_INSTALL_PREFIX])
 -contains(CMAKE_LIB_DIR,"^\\.\\./.*") {
 +contains(CMAKE_LIB_DIR,"^\\.\\./.*")|freebsd {
      CMAKE_LIB_DIR = $$[QT_INSTALL_LIBS]/
      CMAKE_LIB_DIR_IS_ABSOLUTE = True
  } else {
-@@ -67,20 +71,20 @@
+@@ -67,13 +67,13 @@
  }
-
+ 
  CMAKE_BIN_DIR = $$cmakeRelativePath($$[QT_HOST_BINS], $$[QT_INSTALL_PREFIX])
 -contains(CMAKE_BIN_DIR, "^\\.\\./.*") {
 +contains(CMAKE_BIN_DIR, "^\\.\\./.*")|freebsd {
      CMAKE_BIN_DIR = $$[QT_HOST_BINS]/
      CMAKE_BIN_DIR_IS_ABSOLUTE = True
  }
-
+ 
  CMAKE_PLUGIN_DIR = $$cmakeRelativePath($$[QT_INSTALL_PLUGINS], $$[QT_INSTALL_PREFIX])
 -contains(CMAKE_PLUGIN_DIR, "^\\.\\./.*") {
 +contains(CMAKE_PLUGIN_DIR, "^\\.\\./.*")|freebsd {
      CMAKE_PLUGIN_DIR = $$[QT_INSTALL_PLUGINS]/
      CMAKE_PLUGIN_DIR_IS_ABSOLUTE = True
  }
-
- win32:!wince:!static:!staticlib {
-     CMAKE_DLL_DIR = $$cmakeRelativePath($$[QT_INSTALL_BINS], $$[QT_INSTALL_PREFIX])
--    contains(CMAKE_DLL_DIR, "^\\.\\./.*") {
-+    contains(CMAKE_DLL_DIR, "^\\.\\./.*")|freebsd {
-         CMAKE_DLL_DIR = $$[QT_INSTALL_BINS]/
-         CMAKE_DLL_DIR_IS_ABSOLUTE = True
-     }
-@@ -167,7 +171,7 @@
+@@ -167,7 +167,7 @@
        cmake_target_file
-
+ 
      cmake_qt5_plugin_file.files = $$cmake_target_file.output
 -    cmake_qt5_plugin_file.path = $$[QT_INSTALL_LIBS]/cmake/Qt5$${CMAKE_MODULE_NAME}
 +    cmake_qt5_plugin_file.path = $$[QT_INSTALL_PREFIX]/lib/cmake/Qt5$${CMAKE_MODULE_NAME}
      INSTALLS += cmake_qt5_plugin_file
-
+ 
      return()
-@@ -314,7 +318,7 @@
+@@ -314,7 +314,7 @@
      cmake_qt5_module_files.files += $$cmake_macros_file.output
  }
-
+ 
 -cmake_qt5_module_files.path = $$[QT_INSTALL_LIBS]/cmake/Qt5$${CMAKE_MODULE_NAME}
 +cmake_qt5_module_files.path = $$[QT_INSTALL_PREFIX]/lib/cmake/Qt5$${CMAKE_MODULE_NAME}
-
+ 
  # We are generating cmake files. Most developers of Qt are not aware of cmake,
  # so we require automatic tests to be available. The only module which should

commit aa36e9b90504b102879d630dbc715270e72837bd

bsd.qt.mk: Fix _EXTRA_PATCHES_QT4 assignment.
Broken in commit 3e8f56d5c ("Update devel/qt5-qmake").

Mk/bsd.qt.mk
@@ -169,8 +169,8 @@ PLIST_SUB+=		DEBUG="@comment "
 . if ${QT_DIST} == "base" || ${_QT_VERSION:M4*}
 .  if ${_QT_VERSION:M4*}
 _EXTRA_PATCHES_QT4=	${.CURDIR:H:H}/devel/${_QT_RELNAME}/files/extrapatch-src-corelib-global-qglobal.h \
-					${.CURDIR:H:H}/devel/${_QT_RELNAME}/files/extrapatch-libtool
-		${.CURDIR:H:H}/devel/${_QT_RELNAME}/files/extrapatch-config.tests-unix-compile.test \
+					${.CURDIR:H:H}/devel/${_QT_RELNAME}/files/extrapatch-libtool \
+					${.CURDIR:H:H}/devel/${_QT_RELNAME}/files/extrapatch-config.tests-unix-compile.test
 # Patch in proper name for armv6 architecture: https://gcc.gnu.org/ml/gcc-patches/2015-06/msg01679.html
 _EXTRA_PATCHES_QT4+=	${.CURDIR:H:H}/devel/${_QT_RELNAME}/files/extrapatch-armv6
 .  else