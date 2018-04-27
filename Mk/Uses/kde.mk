7d0aa2c7b7fed81d596d76227eea62d4f25024d2
Schaich <alonso@freebsd.org>
2018-04-28 00:14:26 +0200

    Make kde-applications fetchable
    18.04.0 is a stable release

--- Mk/Uses/kde.mk
+++ Mk/Uses/kde.mk
@@ -75,7 +75,7 @@ KDE_FRAMEWORKS_BRANCH?= 	stable
 # Current KDE applications.
 KDE_APPLICATIONS_VERSION?=	18.04.0
 KDE_APPLICATIONS_SHLIB_VER?=	5.8.0
-KDE_APPLICATIONS_BRANCH?=	unstable
+KDE_APPLICATIONS_BRANCH?=	stable
 # Upstream moves old software to Attic/. Specify the newest applications release there.
 # Only the major version is used for the comparison.
 _KDE_APPLICATIONS_ATTIC_VERSION=	16.12.3
