062c07c11ab8749e924d981af4d5327ad029aea7
Tobias C. Berner <tcberner@FreeBSD.org>
2018-10-16 19:54:30 +0200

    Plasma5: upgrade to 5.14.1

--- Mk/Uses/kde.mk
+++ Mk/Uses/kde.mk
@@ -66,7 +66,7 @@ KDE4_APPLICATIONS_VERSION?=	15.04.3
 KDE4_BRANCH?=			stable
 
 # Current KDE desktop.
-KDE_PLASMA_VERSION?=		5.14.0
+KDE_PLASMA_VERSION?=		5.14.1
 KDE_PLASMA_BRANCH?=		stable
 
 # Current KDE frameworks.
