e6e0139d009423086bf4fcef0f1e09f70d2535b1
Tobias C. Berner <tcberner@FreeBSD.org>
2018-12-11 14:10:11 +0100

    poppler-0.7x: backport r22703, r22741, r22745 and additional api change to print/scribus-devel

--- print/scribus-devel/files/patch-scribus_plugins_import_pdf_importpdf.h
+++ print/scribus-devel/files/patch-scribus_plugins_import_pdf_importpdf.h
@@ -1,4 +1,4 @@
---- scribus/plugins/import/pdf/importpdf.h.orig	2018-12-09 00:00:21 UTC
+--- scribus/plugins/import/pdf/importpdf.h.orig	2018-12-11 12:48:12 UTC
 +++ scribus/plugins/import/pdf/importpdf.h
 @@ -81,7 +81,7 @@ class PdfPlug : public QObject (public)
  private:
