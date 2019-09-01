7285b10cbe9f0cda8215a207afc27549b5ddb8bd
Adriaan de Groot <groot@kde.org>
2019-09-01 20:51:39 +0200

    Complete rclone-browser patches

--- net/rclone-browser/files/patch-src_item__model.cpp
+++ net/rclone-browser/files/patch-src_item__model.cpp
@@ -1,6 +1,15 @@
---- src/item_model.cpp.orig	2017-03-11 22:16:36 UTC
-+++ src/item_model.cpp
-@@ -10,7 +10,7 @@ namespace
+--- src/item_model.cpp.orig	2017-03-11 22:16:36.000000000 +0000
++++ src/item_model.cpp	2019-09-01 18:49:37.991420000 +0000
+@@ -2,6 +2,8 @@
+ #include "icon_cache.h"
+ #include "utils.h"
+ 
++#include <algorithm>
++
+ namespace
+ {
+     static void advanceSpinner(QString& text)
+@@ -10,7 +12,7 @@
          QChar current = text[spinnerPos];
          static const QChar spinner[] = { '-', '\\', '|', '/' };
          size_t spinnerCount = sizeof(spinner) / sizeof(*spinner);
@@ -9,7 +18,7 @@
          size_t idx = found - spinner;
          size_t next = idx == spinnerCount - 1 ? 0 : idx + 1;
          text[spinnerPos] = spinner[next];
-@@ -161,7 +161,7 @@ void ItemModel::rename(const QModelIndex& index, const
+@@ -161,7 +163,7 @@
  {
      Item* item = get(index);
      item->name = name;
@@ -18,7 +27,7 @@
      emit dataChanged(index, index, QVector<int>{Qt::DisplayRole});
  }
  
-@@ -182,7 +182,7 @@ QModelIndex ItemModel::addRoot(const QString& name, co
+@@ -182,7 +184,7 @@
      Item* item = new Item();
      item->isFolder = true;
      item->name = name;
@@ -27,7 +36,7 @@
      item->parent = mRoot;
      mRoot->childs.append(item);
  
-@@ -500,7 +500,7 @@ void ItemModel::load(const QPersistentModelIndex& pare
+@@ -500,7 +502,7 @@
              auto it = existing.find(item->name);
              if (it == existing.end())
              {
@@ -36,3 +45,23 @@
                  if (!item->isFolder && mFileIcons)
                  {
                      QString ext = QFileInfo(item->name).suffix();
+@@ -561,8 +563,8 @@
+         }
+     };
+ 
+-    QObject::connect(lsd, static_cast<void(QProcess::*)(int)>(&QProcess::finished), this, rcloneFinished);
+-    QObject::connect(lsl, static_cast<void(QProcess::*)(int)>(&QProcess::finished), this, rcloneFinished);
++    QObject::connect(lsd, QOverload<int, QProcess::ExitStatus>::of(&QProcess::finished), this, rcloneFinished);
++    QObject::connect(lsl, QOverload<int, QProcess::ExitStatus>::of(&QProcess::finished), this, rcloneFinished);
+ 
+     QObject::connect(lsd, &QProcess::readyRead, this, [=]()
+     {
+@@ -617,7 +619,7 @@
+ 
+ void ItemModel::sortRecursive(Item* item, const ItemSorter& sorter)
+ {
+-    qSort(item->childs.begin(), item->childs.end(), sorter);
++    std::sort(item->childs.begin(), item->childs.end(), sorter);
+ 
+     for (auto child : item->childs)
+     {
