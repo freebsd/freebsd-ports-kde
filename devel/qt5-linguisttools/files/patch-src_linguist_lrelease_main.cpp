07fb1ff110bba107809099a1cf4675ff62aeabc5
Adriaan de Groot <groot@kde.org>
2019-09-01 14:16:27 +0200

    Document that this is the same as upstream fix, arrived at independently

--- devel/qt5-linguisttools/files/patch-src_linguist_lrelease_main.cpp
+++ devel/qt5-linguisttools/files/patch-src_linguist_lrelease_main.cpp
@@ -1,3 +1,9 @@
+This is upstream as
+
+https://code.qt.io/cgit/qt/qttools.git/commit/src/linguist/lrelease?h=5.13.1&id=6a832f5474d3b4f789e4bcdebcd425e5ec98423f
+
+in 5.13.1
+
 --- src/linguist/lrelease/main.cpp.orig	2019-09-01 11:53:23 UTC
 +++ src/linguist/lrelease/main.cpp
 @@ -180,7 +180,9 @@ static QStringList translationsFromProjects(const Proj
