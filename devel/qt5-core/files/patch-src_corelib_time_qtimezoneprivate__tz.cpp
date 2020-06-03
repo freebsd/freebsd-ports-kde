46c475e2dbf1f5fdb08480f6b45b17dbe05c09bc
Adriaan de Groot <groot@kde.org>
2020-06-03 22:03:23 +0200

    Take a stab at timezones

---
+++ devel/qt5-core/files/patch-src_corelib_time_qtimezoneprivate__tz.cpp
@@ -0,0 +1,28 @@
+--- src/corelib/time/qtimezoneprivate_tz.cpp.orig	2020-05-11 08:15:08 UTC
++++ src/corelib/time/qtimezoneprivate_tz.cpp
+@@ -1153,6 +1153,25 @@ QByteArray QTzTimeZonePrivate::systemTimeZoneId() cons
+         }
+     }
+ 
++    // On some FreeBSD systems, /etc/localtime is a regular file while 
++    // the actual name is in /var/db/zoneinfo
++    if (ianaId.isEmpty()) {
++        QFile tzif(QStringLiteral("/var/db/zoneinfo"));
++        if (tzif.open(QIODevice::ReadOnly)) {
++            const int maximumTZNameLength = 256;
++            QByteArray tzcontents( tzif.read( maximumTZNameLength ) );
++            if (tzcontents.size() >= 2) {
++                // Shortest TZ would be UTC, or r/z, but there's special
++                // cases "GB" and "US"
++                const int newlineIndex = tzcontents.indexOf('\n');
++                if (newlineIndex < 0) // No newline in file
++                    ianaId = tzcontents;
++                if (newlineIndex > 2) // Newline, chop it off
++                    ianaId = tzcontents.left(newlineIndex-1);
++            }
++        }
++    }
++
+     // Some systems (e.g. uClibc) have a default value for $TZ in /etc/TZ:
+     if (ianaId.isEmpty()) {
+         QFile zone(QStringLiteral("/etc/TZ"));
