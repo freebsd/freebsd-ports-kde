6dc2056bd19938f897f3fc0703e8dfc0be2985d0
Jason E. Hale <jhale@FreeBSD.org>
2025-12-04 20:29:31 -0500

    Qt6: Update distinfos for 6.10.1

--- Mk/Uses/qt.mk
+++ Mk/Uses/qt.mk
@@ -23,8 +23,8 @@ _QT_MK_INCLUDED=	qt.mk
 # Qt versions currently supported by the framework.
 _QT_SUPPORTED?=		5 6
 QT5_VERSION?=		5.15.18
-QT6_VERSION?=		6.10.0
-PYSIDE6_VERSION?=	6.10.0
+QT6_VERSION?=		6.10.1
+PYSIDE6_VERSION?=	6.10.1
 
 # Support for intermediate Qt6 releases. This partially defines
 # _QT6_MASTER_SITE_SUBDIR and would probably be better in qt-dist.mk,
